81b34e2736eaeb7252225d0801616576f929c49e
Seorish <seojin0774@naver.com>
2022-07-30 03:40:06 +0900

    프론트작업

--- Re_Toilet/urls.py
+++ Re_Toilet/urls.py
@@ -21,5 +21,6 @@ from RetoApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-
+    path('review/', views.review, name="review"),
+    path('search/', views.search, name="search")
 ]
--- RetoApp/views.py
+++ RetoApp/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+import requests
+import json
+
+
+secret_key ='sQi%2FzMQ3vue0d1rD%2FvPtCe2OgezQc37UYHrqZnNakgkK%2B1ugwK%2BWAkj74lu4Kpz1WbP3s%2FlJYG%2B9Utm%2BCZVSJg%3D%3D'
+
 
 
 def home(request):
-    return render(request, "index.html")
 
+    url ='http://api.data.go.kr/openapi/tn_pubr_public_toilet_api?serviceKey='+secret_key+'&pageNo=0&numOfRows=100&type=json'
+
+    response = requests.get(url)
+    resdata = response.text
+    obj = json.loads(resdata)
+    response = requests.get(url)
+    resdata = response.text
+    obj = json.loads(resdata)
+    docs = obj['response']
+    docs_t = docs['body']
+    final = docs_t['items']
+
+    
+    return render(request, "index.html", {'final' : final})
+
+
+
+
+def review(request):
+    return render(request, "review.html")
+ 
+def search(request):
+    return render(request, "search.html")
+ 
